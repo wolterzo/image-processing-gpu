83135f8d7d9078a3a0ad75f9ccd5cdf73d062969
Victoria Tsou <vt6666@gmail.com>
2016-04-29 13:51:43 -0500

    get pixel data and add white image

--- image_transform.cu
+++ image_transform.cu
@@ -6,7 +6,7 @@ using namespace Magick;
 
 int main (int argc, char** argv) {
   InitializeMagick(*argv);
-  
+
   Image image;
   try {
     // Read a file into image object
@@ -16,17 +16,27 @@ int main (int argc, char** argv) {
     cout << "Caught exception: " << error_.what() << endl;
     return 1;
   }
-  PixelPacket* cpu_packet = image.getPixels(0, 0, image.columns, image.rows);
+  Pixels view(image);
+   *(view.get(108,94,1,1)) = Color("red");
+
+   int width = image.columns();
+   int height = image.rows();
+   int range = 256;
+   PixelPacket* cpu_packet = image.getPixels(0, 0, width, height);
+   
+   // Color color = cpu_packet[0];
+   // cout << (color.redQuantum() / range) << endl;
  
-  PixelPacket* gpu_packet;
-  if(cudaMalloc(&gpu_packet, sizeof(PixelPacket) * image.columns * image.rows) != cudaSuccess) {
+   PixelPacket* gpu_packet;
+   if(cudaMalloc(&gpu_packet, sizeof(PixelPacket) * width * height) != cudaSuccess) {
     fprintf(stderr, "Failed to create image for the gpu\n");
     exit(2);
   }
   
   //Copy contents from cpu to gpu
-  if(cudaMemcpy(gpu_packet, cpu_packet, sizeof(PixelPacket) * image.columns * image.rows , cudaMemcpyHostToDevice) != cudaSuccess) {
+   if(cudaMemcpy(gpu_packet, cpu_packet, sizeof(PixelPacket) *  width * height, cudaMemcpyHostToDevice) != cudaSuccess) {
     fprintf(stderr, "Failed to copy image from CPU to the GPU\n");
       }
+  
   return 0;
 }
